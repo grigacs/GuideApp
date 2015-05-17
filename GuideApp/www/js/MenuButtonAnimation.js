08e4e2bf08f33cd30478fdcf7ed172e819f1ff75
grigacs <grigacs91@gmail.com>
2015-05-17 18:47:21 +0200

    fix page navigation

--- GuideApp/www/js/MenuButtonAnimation.js
+++ GuideApp/www/js/MenuButtonAnimation.js
@@ -1,32 +1,50 @@
-$(document).ready(function () {
+function menuAnimation(thisPage) {
+
     var menuAnimation = false;
+        $('.navIconAnimation').on("tap", function (e) {
+            e.preventDefault();
+            $('#overlayPanel' + thisPage).panel('open');
+            if (menuAnimation == false) {
+                $(this).addClass('open');
+                menuAnimation = true;
+            }
+            else {
+                $('#overlayPanel' + thisPage).panel('close');
+                $(this).removeClass('open');
+                menuAnimation = false;
+            }
+        });
 
-    $('#nav-icon1').on("tap", function () {
-        $('#overlayPanel').panel('open');
-        if (menuAnimation == false) {
-            $(this).addClass('open');
+        $(window).on("swiperight", function () {
+            $('#overlayPanel' + thisPage).panel('open');
+            $('.navIconAnimation').addClass('open');
             menuAnimation = true;
-        }
-        else{
-            $('#overlayPanel').panel('close');
-            $(this).removeClass('open');
+        });
+
+        $(window).on("swipeleft", function () {
+            $('#overlayPanel' + thisPage).panel('close');
+            $('.navIconAnimation').removeClass('open');
             menuAnimation = false;
-        }
-    });
+        });
+
+        $('.resetMenuAnimation').on("tap", function () {
+            $('#overlayPanel' + thisPage).panel('close');
+            $('.navIconAnimation').removeClass('open');
+            menuAnimation = false;
+        });
 
 
+}
 
-    $(window).on("swiperight", function () {
-        $('#overlayPanel').panel('open');
-        $('#nav-icon1').addClass('open');
-        menuAnimation = true;
-    });
 
-    $(window).on("swipeleft", function () {
-        $('#overlayPanel').panel('close');
-        $('#nav-icon1').removeClass('open');
-        menuAnimation = false;
-    });
-
+$(document).on('pagecreate', function () {
+   // var PageID = $.mobile.activePage.attr('id');
+        menuAnimation('indexPage');
 });
+
+
+
+
+
+
 
--- GuideApp/www/js/ajaxDataLoader.js
+++ GuideApp/www/js/ajaxDataLoader.js
@@ -1,17 +1,15 @@
-$(document).ready(function()
-{
-
+function getDataFromJson(){
     $.getJSON("http://192.168.1.7/ajaxProbaJson/index.php", function(result){
         $.each(result, function(i, data){
-                // all data is Object (result[Object object])
-                for(var j=0;j<data.length;j++) {
-                    $("#result").append(data[j]['fname'] + " " + data[j]['lname'] + "<br />");
-                }
-                // you can see data structure at console log
-                console.log(data)
+            // all data is Object (result[Object object])
+            for(var j=0;j<data.length;j++) {
+                $(".result").append(data[j]['fname'] + " " + data[j]['lname'] + "<br />");
+            }
+            // you can see data structure at console log
+            console.log(data)
         });
         // test for events
-        $("#result").append("<a id='clickMe' href='http://192.168.1.7/ajaxProbaJson/index.php'>FROM</a>");
+        $(".result").append("<a id='clickMe' href='http://192.168.1.7/ajaxProbaJson/index.php'>FROM</a><br/><a id='clickMe2' href='http://192.168.1.7/ajaxProbaJson/index.php'>FROM</a>");
 
         // end
     });
@@ -19,8 +17,25 @@ $(document).ready(function()
     // test code how can add event handler at appended elements
     $(document).on('tap','#clickMe',function(e){
         e.preventDefault();
+        alert('Work')
+    });
+
+    $(document).on('tap','#clickMe2',function(){
         alert('Work');
     });
     // test code end
+}
 
+
+
+
+$(document).on('pagecreate','#indexPage', function()
+{
+    getDataFromJson();
 });
+
+
+
+
+
+
