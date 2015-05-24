dedcaa854abcff13ed1365fea80dba8a0f772873
PeterB22 <peter.borcsok22@gmail.com>
2015-05-24 16:41:27 +0200

    event listing under construction

---
+++ GuideApp/www/js/GetEvents.js
@@ -0,0 +1,56 @@
+
+var titles=[];
+var contents=[];
+
+function getDataFromJson(){
+
+    var lang=localStorage.getItem("Mylanguage");
+
+    $.getJSON("http://192.168.0.8/GuideApp/index.php?language="+lang,function(result){
+            console.log(result);
+        $.each(result, function(i, data){
+            // all data is Object (result[Object object])
+
+            for(var j=0;j<data.length;j++) {
+                    titles.push(data[j]['title']);
+                  contents.push(data[j]['content']);
+            }
+            titles= $.grep(titles,function(n){return(n)});
+            contents= $.grep(contents,function(n){return(n)});
+
+        });
+
+    });
+
+
+    $(document).on('tap','#clickMe',function(e){
+        e.preventDefault();
+        alert('Work')
+    });
+
+
+
+}
+
+
+function listEvents(){
+    for(var j=0;j<titles.length;j++)
+    {
+        var d=document.createElement('div');
+        $(d).addClass("oneEvent");
+        $(d).html("<div class='main-title'>" + titles[j] + "</div>");
+        $(d).appendTo($('.event-content'));
+        $(d).append("<div class='texts'><p>" +contents[j] + "</p></div>");
+
+    }
+}
+
+
+
+
+$(document).on('pageshow','#eventPage', function()
+{
+    getDataFromJson();
+    listEvents();
+});
+
